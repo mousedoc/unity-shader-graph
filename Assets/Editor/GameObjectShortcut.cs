c95c042a30850955695d1928d7e33c331944ff16
mousedoc <sangyun3631@gmail.com>
2022-12-16 18:59:47 +0900

    Add editor shortcut

---
+++ Assets/Editor/GameObjectShortcut.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using System.Diagnostics.CodeAnalysis;
+
+public class GameObjectShortcut
+{
+    [MenuItem("Tools/GameObject/Active DeActive %g", priority = 12)]
+    [SuppressMessage("CodeQuality", "IDE0051:사용되지 않는 private 멤버 제거", Justification = "Called by Unity editor")]
+    private static void ChangeActiveState()
+    {
+        if (Selection.gameObjects == null)
+            return;
+
+        var selected = Selection.gameObjects;
+        foreach (var elem in selected)
+        {
+            elem.SetActive(!elem.activeSelf);
+            EditorUtility.SetDirty(elem);
+        }
+    }
+}
