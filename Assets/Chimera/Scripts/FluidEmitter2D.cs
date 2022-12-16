045be6f370094942ff9468e33e1d2fe4460d4b60
mousedoc <sangyun3631@gmail.com>
2022-12-16 16:02:53 +0900

    soeme shits > GPU Fluid Sim > Voronoi Terrain

---
+++ Assets/Chimera/Scripts/FluidEmitter2D.cs
@@ -0,0 +1,163 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chimera
+{
+	public class FluidEmitter2D : MonoBehaviour
+	{
+		public enum EmitterShape { Directional, Circular }
+		[Header("Shape")]
+		public EmitterShape shape = EmitterShape.Directional;
+		public bool normalizeDirection = false;
+
+		public enum PositionMapping { XY, XZ, YZ }
+		[Header("Position Mapping")]
+		public PositionMapping positionMapping = PositionMapping.XY;
+		[Tooltip("Size of your fluid area in meters.")] public Vector2 size = Vector2.one;
+		[Tooltip("Is your emitter centered on its origin ?")] public bool isCentered = false;
+
+		[Header("Fluid Emitter")]
+		public string fluidControllerID;
+		public float force = 1.0f;
+		public float forceRadius = 0.1f;
+
+		[Header("Color Emitter")]
+		public string advectionControllerID;
+		public Color color = Color.white;
+		public float intensity = 1.0f;
+		public float colorRadius = 0.05f;
+
+		[Header("Gizmos")]
+		public bool showGizmos = true;
+
+		[HideInInspector] public Vector2 position = Vector2.zero;
+		[HideInInspector] public Vector2 direction = Vector2.one * 0.1f;
+
+		private FluidTextureController _fluidController;
+		private AdvectionTextureController _advectionController;
+
+		private Vector3 _direction3D = Vector3.zero;
+		private Vector3 _previousPosition;
+
+		private bool _isPreviousPositionValid = false;
+
+		private void OnEnable() {
+
+			AddEmitter();
+			UpdateEmitter();
+		}
+
+		private void Update() {
+
+			if(_fluidController.updateMode == FluidTextureController.UpdateMode.Update)
+				UpdateEmitter();
+		}
+
+		private void FixedUpdate() {
+
+			if (_fluidController.updateMode == FluidTextureController.UpdateMode.FixedUpdate)
+				UpdateEmitter();
+		}
+
+		private void LateUpdate() {
+
+			if (_fluidController.updateMode == FluidTextureController.UpdateMode.LateUpdate)
+				UpdateEmitter();
+		}
+
+		private void OnDisable() {
+
+			RemoveEmitter();
+		}
+
+		private void OnDrawGizmos() {
+
+			if (!showGizmos)
+				return;
+
+			Gizmos.color = Color.yellow;
+
+			switch (positionMapping) {
+				case PositionMapping.XY:
+					_direction3D = Vector3.ProjectOnPlane(transform.forward, Vector3.back);
+					break;
+				case PositionMapping.XZ:
+					_direction3D = Vector3.ProjectOnPlane(transform.forward, Vector3.up);
+					break;
+				case PositionMapping.YZ:
+					_direction3D = Vector3.ProjectOnPlane(transform.forward, Vector3.right);
+					break;
+			}
+
+			Gizmos.DrawLine(transform.position, transform.position + _direction3D * force * 0.5f);
+		}
+
+		void AddEmitter() {
+
+			foreach (var controller in FindObjectsOfType<FluidTextureController>()) {
+				if (controller.ID == fluidControllerID) {
+					_fluidController = controller; break;
+				}
+			}
+
+			if (_fluidController)
+				_fluidController.AddEmitter(this);
+
+			foreach (var controller in FindObjectsOfType<AdvectionTextureController>()) {
+				if (controller.ID == advectionControllerID) {
+					_advectionController = controller; break;
+				}
+			}
+
+			if (_advectionController)
+				_advectionController.AddEmitter(this);
+		}
+
+		void UpdateEmitter() {
+
+			if (!_isPreviousPositionValid) {
+				_previousPosition = transform.position;
+				_isPreviousPositionValid = true;
+				return;
+			}
+
+			_direction3D = transform.position - _previousPosition;
+
+			switch (positionMapping) {
+				case PositionMapping.XY:
+					position.x = isCentered ? transform.position.x / size.x + 0.5f : transform.position.x / size.x;
+					position.y = isCentered ? transform.position.y / size.y + 0.5f : transform.position.y / size.y;
+					_direction3D = normalizeDirection ? Vector3.ProjectOnPlane(_direction3D, Vector3.back).normalized : Vector3.ProjectOnPlane(_direction3D, Vector3.back);
+					direction.x = _direction3D.x;
+					direction.y = _direction3D.y;
+					break;
+				case PositionMapping.XZ:
+					position.x = isCentered ? transform.position.x / size.x + 0.5f : transform.position.x / size.x;
+					position.y = isCentered ? transform.position.z / size.y + 0.5f : transform.position.z / size.y;
+					_direction3D = normalizeDirection ? Vector3.ProjectOnPlane(_direction3D, Vector3.up).normalized : Vector3.ProjectOnPlane(_direction3D, Vector3.up);
+					direction.x = _direction3D.x;
+					direction.y = _direction3D.z;
+					break;
+				case PositionMapping.YZ:
+					position.x = isCentered ? transform.position.y / size.x + 0.5f : transform.position.y / size.x;
+					position.y = isCentered ? transform.position.z / size.y + 0.5f : transform.position.z / size.y;
+					_direction3D = normalizeDirection ? Vector3.ProjectOnPlane(_direction3D, Vector3.right).normalized : Vector3.ProjectOnPlane(_direction3D, Vector3.right);
+					direction.x = _direction3D.y;
+					direction.y = _direction3D.z;
+					break;
+			}
+
+			_previousPosition = transform.position;
+		}
+
+		void RemoveEmitter() {
+
+			if (_fluidController)
+				_fluidController.RemoveEmitter(this);
+
+			if (_advectionController)
+				_advectionController.RemoveEmitter(this);
+		}
+	}
+}
---
+++ Assets/Chimera/Scripts/FluidTextureController.cs
@@ -0,0 +1,207 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chimera
+{
+	public class FluidTextureController : MonoBehaviour
+	{
+		[Header("Fluid Controller ID")]
+		public string ID;
+
+		[Header("Custom Render Textures")]
+		public CustomRenderTexture fluidTexture;
+
+		[Header("Fluid Parameters")]
+		[Range(0, 10)] public int updatesPerFrame = 4;
+
+		public enum UpdateMode { Update, FixedUpdate, LateUpdate }
+		public UpdateMode updateMode = UpdateMode.Update;
+
+		[Header("Debug")]
+		public bool reinitialize = false;
+		public bool showLogs = false;
+
+		public struct Emitter
+		{
+			public Vector2 position;
+			public Vector2 direction;
+			public float force;
+			public float radius;
+			public float shape;
+		}
+
+		private const int _fluidEmitterSize = 7 * sizeof(float);
+
+		private ComputeBuffer _emittersBuffer;
+		private List<FluidEmitter2D> _emittersList;
+		private Emitter[] _emittersArray;
+
+		private Material fluidMaterial;
+
+		private bool _initialized = false;
+
+		#region MonoBehaviour Functions
+
+		private void OnEnable() {
+
+			if (!_initialized)
+				Initialize();
+		}
+
+		void Update() {
+			if(updateMode == UpdateMode.Update)
+				UpdateFluid();
+		}
+
+		void FixedUpdate() {
+			if (updateMode == UpdateMode.FixedUpdate)
+				UpdateFluid();
+		}
+
+		void LateUpdate() {
+			if (updateMode == UpdateMode.LateUpdate)
+				UpdateFluid();
+		}
+
+		private void OnDisable() {
+
+			if (_initialized)
+				CleanUp();
+		}
+
+		#endregion
+
+		void Initialize() {
+
+			fluidMaterial = fluidTexture.material;
+
+			//Initialize emitters
+			InitializeEmitters();
+
+			//Initialize fluid texture
+			fluidTexture.Initialize();
+
+			_initialized = true;
+		}
+
+		void UpdateFluid() {
+
+			if (!_initialized) {
+				Initialize();
+				return;
+			}
+
+			//Update shader time
+			fluidMaterial.SetFloat("_AbsoluteTime", Time.time);
+
+			//Update emitters
+			UpdateEmittersBuffer();
+
+			//Update fluid texture
+			fluidTexture.Update(updatesPerFrame);
+
+			//Debug
+			if (reinitialize) {
+				fluidTexture.Initialize();
+				reinitialize = false;
+			}
+
+			if (showLogs)
+				DebugEmittersList();
+		}
+
+		void CleanUp() {
+
+			ReleaseEmittersBuffer();
+
+			_initialized = false;
+		}
+
+		#region Emitters
+
+		void InitializeEmitters() {
+
+			CreateEmittersList();
+			CreateEmittersArray();
+			CreateEmittersBuffer();
+		}
+
+		void CreateEmittersList() {
+
+			_emittersList = new List<FluidEmitter2D>();
+		}
+
+		void CreateEmittersArray() {
+
+			_emittersArray = _emittersList.Count > 0 ? new Emitter[_emittersList.Count] : new Emitter[1];
+		}
+
+		void CreateEmittersBuffer() {
+
+			if (_emittersBuffer != null)
+				_emittersBuffer.Release();
+
+			_emittersBuffer = _emittersList.Count > 0 ? new ComputeBuffer(_emittersList.Count, _fluidEmitterSize) : new ComputeBuffer(1, _fluidEmitterSize);
+
+			UpdateEmittersBuffer();
+		}
+
+		void UpdateEmittersArray() {
+
+			if (_emittersArray.Length != _emittersList.Count)
+				CreateEmittersArray();
+
+			for (int i = 0; i < _emittersList.Count; i++) {
+				_emittersArray[i].position = _emittersList[i].position;
+				_emittersArray[i].direction = _emittersList[i].direction;
+				_emittersArray[i].force = _emittersList[i].force;
+				_emittersArray[i].radius = _emittersList[i].forceRadius;
+				_emittersArray[i].shape = _emittersList[i].shape == FluidEmitter2D.EmitterShape.Directional ? 0 : 1;
+			}
+		}
+
+		void UpdateEmittersBuffer() {
+
+			UpdateEmittersArray();
+
+			_emittersBuffer.SetData(_emittersArray);
+
+			fluidMaterial.SetBuffer("_EmittersBuffer", _emittersBuffer);
+			fluidMaterial.SetInt("_EmittersCount", _emittersList.Count);
+		}
+
+		void ReleaseEmittersBuffer() {
+
+			_emittersBuffer.Release();
+		}
+
+		public void AddEmitter(FluidEmitter2D emitter) {
+
+			if (!_initialized)
+				Initialize();
+
+			_emittersList.Add(emitter);
+
+			CreateEmittersBuffer();
+		}
+
+		public void RemoveEmitter(FluidEmitter2D emitter) {
+
+			_emittersList.Remove(emitter);
+
+			CreateEmittersBuffer();
+		}
+
+		void DebugEmittersList() {
+
+			Debug.Log("EmittersListCount = " + _emittersList.Count);
+
+			for (int i = 0; i < _emittersList.Count; i++) {
+				Debug.Log("Emitter " + i + " : position = " + _emittersList[i].position + "; direction = " + _emittersList[i].direction + "; force = " + _emittersList[i].force);
+			}
+		}
+
+		#endregion
+	}
+}
