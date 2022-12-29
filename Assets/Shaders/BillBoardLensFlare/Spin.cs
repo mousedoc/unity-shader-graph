b4cd18586281fcef63d05a80e8e04a60c7f5c10c
mousedoc <sangyun3631@gmail.com>
2022-12-29 18:58:06 +0900

    qqq

---
+++ Assets/Shaders/BillBoardLensFlare/Spin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    public float speed = 3f;
+    
+    private void Update()
+    {
+        var yRot = transform.localRotation.eulerAngles + new Vector3(0, speed * Time.deltaTime, 0);
+        transform.localRotation = Quaternion.Euler(yRot);
+    }
+}
