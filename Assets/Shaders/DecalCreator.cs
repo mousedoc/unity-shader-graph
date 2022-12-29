34e0fc69eaed83b4df9f9471baaa1c578d052212
mousedoc <sangyun3631@gmail.com>
2022-12-29 15:42:10 +0900

    Add ScreenSpace decal sample

---
+++ Assets/Shaders/DecalCreator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class DecalCreator : MonoBehaviour
+{
+    public GameObject targetDecal;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hitInfo) == false)
+                return;
+
+            if (targetDecal == null)
+                return;
+
+            var decal = GameObject.Instantiate(targetDecal);
+            var tr = decal.transform;
+            tr.position = hitInfo.point;
+            tr.rotation = Quaternion.Euler(ray.direction);
+            tr.localScale = Vector3.one;
+        }
+    }
+}
